afcee89630fbd51469fefc40d2947f14a6147698
nirvanashub <99236286+nirvanashub@users.noreply.github.com>
2022-03-21 15:51:05 +0530

    commit

---
+++ variables/array/elseif.js
@@ -0,0 +1,10 @@
+const number = 20;
+if (number == 19) {
+    console.log('number is 19')
+} 
+else if(number == 20){
+    console.log('number is 20')
+}
+else {
+    console.log('number is not 19')
+}
