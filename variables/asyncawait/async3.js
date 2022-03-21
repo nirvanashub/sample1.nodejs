c72f022e260d14afb395890f7b1eb41121f87de3
nirvanashub <99236286+nirvanashub@users.noreply.github.com>
2022-03-21 15:54:02 +0530

    commit

---
+++ variables/asyncawait/async3.js
@@ -0,0 +1,6 @@
+async function foo() {
+    const p1 = new Promise((resolve) => setTimeout(() => resolve('1'), 1000))
+    const p2 = new Promise((_,reject) => setTimeout(() => reject('2'), 500))
+    const results = [await p1, await p2] // Do not do this! Use Promise.all or Promise.allSettled instead.
+ }
+ foo().catch(() => {}) // Attempt to swallow all errors...
